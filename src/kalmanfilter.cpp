c54d28aa22a34ad1dc3e1f60ed16190c563f80ee
Mahee8877 <maheedharkorimi@gmail.com>
2024-07-28 14:04:42 +0100

    Initial commit

--- src/kalmanfilter.cpp
+++ src/kalmanfilter.cpp
@@ -13,7 +13,7 @@
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
 constexpr bool INIT_ON_FIRST_PREDICTION = true;
 constexpr double INIT_POS_STD = 0;
-constexpr double INIT_VEL_STD = 15;
+constexpr double INIT_VEL_STD = 0;
 constexpr double ACCEL_STD = 0.1;
 constexpr double GPS_POS_STD = 3.0;
 // -------------------------------------------------- //
@@ -35,7 +35,17 @@ void KalmanFilter::predictionStep(double dt)
 
             // Assume the initial position is (X,Y) = (0,0) m
             // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
+
+            //state << 0, 0, 0, 0;
             state << 0, 0, 5.0*cos(M_PI/4), 5.0*sin(M_PI/4);
+
+            const double init_pos_std = INIT_POS_STD;
+            const double init_vel_std = INIT_VEL_STD;
+
+            cov(0,0) = INIT_POS_STD*INIT_POS_STD;
+            cov(1,1) = INIT_POS_STD*INIT_POS_STD;
+            cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
 
             setState(state);
             setCovariance(cov);
@@ -52,7 +62,18 @@ void KalmanFilter::predictionStep(double dt)
         // Hint: You can use the constants: ACCEL_STD
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE
+        MatrixXd F = Matrix4d();
+        F << 1,0,dt,0,0,1,0,dt,0,0,1,0,0,0,0,1;
 
+        MatrixXd Q = Matrix2d::Zero();
+        Q(0,0) = (ACCEL_STD*ACCEL_STD);
+        Q(1,1) = (ACCEL_STD*ACCEL_STD);
+
+        MatrixXd L = MatrixXd(4,2);
+        L << (0.5*dt*dt),0,0,(0.5*dt*dt),dt,0,0,dt;
+
+        state = F * state;
+        cov = F * cov * F.transpose() + L * Q * L.transpose();
 
         // ----------------------------------------------------------------------- //
 
@@ -74,6 +95,23 @@ void KalmanFilter::handleGPSMeasurement(GPSMeasurement meas)
         // Hint: You can use the constants: GPS_POS_STD
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE 
+
+        VectorXd z = Vector2d();
+        MatrixXd H = MatrixXd(2,4);
+        MatrixXd R = Matrix2d::Zero();
+
+        z << meas.x,meas.y;
+        H << 1,0,0,0,0,1,0,0;
+        R(0,0) = GPS_POS_STD*GPS_POS_STD;
+        R(1,1) = GPS_POS_STD*GPS_POS_STD;
+
+        VectorXd z_hat = H * state;
+        VectorXd y = z - z_hat;
+        MatrixXd S = H * cov * H.transpose() + R;
+        MatrixXd K = cov*H.transpose()*S.inverse();
+
+        state = state + K*y;
+        cov = (MatrixXd::Identity(4,4) - K*H) * cov;
 
 
         // ----------------------------------------------------------------------- //
